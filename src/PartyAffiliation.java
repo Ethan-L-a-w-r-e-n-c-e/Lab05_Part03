da3ee31daa24bdfec11256093cc694012e48f799
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-09-24 08:48:27 -0400

    Initial commit

---
+++ src/PartyAffiliation.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String party;
+
+        System.out.println("What is your party [D,R,I]");
+        party = scan.nextLine();
+        
+    }
+}
