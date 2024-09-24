331d31dde0747f2bc1ab9ee36f1d7f77f40266d0
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-09-24 08:49:17 -0400

    I hate Github

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
 
-public class Main {
+public class PartyAffiliation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String party;
 
         System.out.println("What is your party [D,R,I]");
         party = scan.nextLine();
-        
+        switch (party) {
+            case "D":
+                System.out.println("You get a Democratic Donkey");
+                break;
+            case "R":
+                System.out.println("You get a Republican Elephant");
+                break;
+            case "I":
+                System.out.println("You get an Independent Person");
+                break;
+            default:
+                System.out.println("You are Communists ");
+                break;
+        }
     }
 }
